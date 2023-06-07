b3e0f18d1762e3314658c318ffed3550da8c8fcb
ANKouwenhoven <arnoudkouwenhoven345@gmail.com>
2023-06-08 00:24:43 +0200

    Added some definitions to LineSensorController.cpp

--- main/LineSensorController.cpp
+++ main/LineSensorController.cpp
@@ -1,5 +1,13 @@
 #include "LineSensorController.h"
 
+#define LINE_VALUE_BLACK 1000
+#define LINE_VALUE_GREEN 200
+#define LINE_VALUE_BROWN 330
+#define LINE_VALUE_GREY 500
+#define LINE_VALUE_EMPTY 80
+
+#define NUMBER_OF_SENSORS 5
+
 LineSensorController() {}
 
 ~LineSensorController() {}
